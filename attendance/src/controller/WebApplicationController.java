22b7e3a2f87ca77264674d010a882d4b9ba3d6b2
ys19304019 <ys19304019@ga.tera-house.ac.jp>
2021-01-28 11:12:52 +0900

    WebApplicationController修正

--- attendance/src/controller/WebApplicationController.java
+++ attendance/src/controller/WebApplicationController.java
@@ -16,7 +16,7 @@ import exception.ApplicationException;
 
 public class WebApplicationController implements ApplicationController {
     public RequestContext getRequest(Object request){
-        RequestContext reqc = (RequestContext) ContextFactory.getContext("webreq");
+        RequestContext reqc = (RequestContext)ContextFactory.getContext("webreq");
         reqc.setRequest(request);
         return reqc;
     }
