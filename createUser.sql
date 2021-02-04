6c897ee7d98e36cfb317a173277c9fe37418d0f5
rt19304023 <rt19304023@ga.tera-house.ac.jp>
2021-02-05 03:01:06 +0900

    test data upgrade

--- createUser.sql
+++ createUser.sql
@@ -3,6 +3,6 @@ CREATE user attendance
 	IDENTIFIED BY attendance
 ;
 
-GRANT CONNECT,RESOURCE TO attendance;
+GRANT CONNECT,RESOURCE,CREATE VIEW TO attendance;
 
 ALTER USER attendance quota unlimited ON USERS;
