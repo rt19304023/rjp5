7c6d9fdba576a65cbba5df23577e839008be1978
Sak <54879453+S4K08@users.noreply.github.com>
2021-02-15 09:50:08 +0900

    Update dropTable.sql

--- mysql/attendance_sql/dropTable.sql
+++ mysql/attendance_sql/dropTable.sql
@@ -1,15 +1,24 @@
 
-DROP TABLE department CASCADE CONSTRAINTS;
+DROP VIEW IF EXISTS login_select;
 
-DROP TABLE employee_list CASCADE CONSTRAINTS;
+DROP VIEW IF EXISTS list_select;
 
-DROP TABLE employee_secret CASCADE CONSTRAINTS;
+DROP VIEW IF EXISTS time_select;
 
-DROP TABLE assignment CASCADE CONSTRAINTS;
+DROP VIEW IF EXISTS secret_select;
 
-DROP TABLE time_sheets CASCADE CONSTRAINTS;
+DROP VIEW IF EXISTS dept_total_time;
 
-DROP SEQUENCE employeeid;
+DROP VIEW IF EXISTS time_per_month;
+
+DROP TABLE IF EXISTS time_sheets;
+
+DROP TABLE IF EXISTS assignment;
+
+DROP TABLE IF EXISTS employee_secret;
+
+DROP TABLE IF EXISTS employee_list;
+
+DROP TABLE IF EXISTS department;
 
 commit;
-
